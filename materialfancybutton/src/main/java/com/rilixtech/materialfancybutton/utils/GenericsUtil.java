9a9ec29a040e19262b838cee547120113486f7a7
elaitenstile <47279543+elaitenstile@users.noreply.github.com>
2021-06-28 16:19:03 +0530

    Reverted gradle file changes

--- materialfancybutton/src/main/java/com/rilixtech/materialfancybutton/utils/GenericsUtil.java
+++ materialfancybutton/src/main/java/com/rilixtech/materialfancybutton/utils/GenericsUtil.java
@@ -2,11 +2,14 @@ package com.rilixtech.materialfancybutton.utils;
 
 
 import ohos.app.Context;
+import ohos.global.resource.Resource;
+import ohos.global.systemres.ResourceTable;
 
 public class GenericsUtil {
 
   public static final String DEFINE_FONT_PREFIX = "define_mfb_font_";
-  public static final String MODULE_PREFIX = "com.example.%s";
+  public static final String MODULE_PREFIX = "com.rilixtech.%s";
+
 
   //TODO This code logic may have to be completely re written.
   // Inferred usage: Dynamically loads all ITypefaces from respective classes if the corresponding define_mfb_font_ is present
