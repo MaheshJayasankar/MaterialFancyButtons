eebcce32a71ec01841eea91f657a85a2589bb231
HaritJaiswal <haritjaiswal2000@gmail.com>
2021-06-29 11:41:44 +0530

    Added Ionicons and MaterialDesignIconic modules

---
+++ ionicons_typeface/src/test/java/com/example/ionicons_typeface/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.ionicons_typeface;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
---
+++ material_design_iconic_typeface/src/test/java/com/example/material_design_iconic_typeface/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.material_design_iconic_typeface;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
