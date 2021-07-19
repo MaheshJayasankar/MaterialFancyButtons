8848b30b5f7ec6ac97dbf87ca8f276649a95e680
elaitenstile <47279543+elaitenstile@users.noreply.github.com>
2021-07-19 15:38:45 +0530

    Sample App now compliant with checkstyle/sonarqube

--- entry/src/main/java/com/rilixtech/materialfancybuttons/ListItemProvider.java
+++ entry/src/main/java/com/rilixtech/materialfancybuttons/ListItemProvider.java
@@ -1,38 +1,61 @@
+/*
+ * Copyright (C) 2020-21 Application Library Engineering Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rilixtech.materialfancybuttons;
 
-import com.rilixtech.materialfancybuttons.slice.MainAbilitySlice;
 import ohos.aafwk.ability.AbilitySlice;
-import ohos.agp.components.*;
-
+import ohos.agp.components.BaseItemProvider;
+import ohos.agp.components.Component;
+import ohos.agp.components.ComponentContainer;
+import ohos.agp.components.LayoutScatter;
+import ohos.agp.components.Text;
+import com.rilixtech.materialfancybuttons.slice.MainAbilitySlice;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A simple implementation of the BaseItemProvider class to handle {@link MainAbilitySlice.NameLayout} items.
+ */
 public class ListItemProvider extends BaseItemProvider {
 
     private final List<String> itemNames;
-    private final int ITEM_COUNT;
-
-
-
+    private final int itemCount;
     private final List<MainAbilitySlice.NameLayout> list;
     private final AbilitySlice slice;
 
+    /** Default constructor for the ListItemProvider class.
+     *
+     * @param list The List of {@link MainAbilitySlice.NameLayout} items that are to be displayed.
+     * @param slice The AbliitySlice utilizing this object.
+     */
     public ListItemProvider(List<MainAbilitySlice.NameLayout> list, AbilitySlice slice) {
         this.list = list;
         this.slice = slice;
 
-        if (slice instanceof MainAbilitySlice){
+        if (slice instanceof MainAbilitySlice) {
             List<String> nameLayoutNames = new ArrayList<>();
             for (MainAbilitySlice.NameLayout nameLayoutObj : list) {
                 String name = nameLayoutObj.getName();
                 nameLayoutNames.add(name);
             }
             itemNames = nameLayoutNames;
-        }
-        else{
+        } else {
             itemNames = new ArrayList<>();
         }
-        ITEM_COUNT = itemNames.size();
+        itemCount = itemNames.size();
     }
 
     @Override
@@ -42,10 +65,11 @@ public class ListItemProvider extends BaseItemProvider {
 
     @Override
     public Object getItem(int position) {
-        if (position < ITEM_COUNT)
+        if (position < itemCount) {
             return list.get(position);
-        else
+        } else {
             return list.get(list.size() - 1);
+        }
     }
 
     @Override
--- entry/src/main/java/com/rilixtech/materialfancybuttons/slice/ProgramButtonsSlice.java
+++ entry/src/main/java/com/rilixtech/materialfancybuttons/slice/ProgramButtonsSlice.java
@@ -1,31 +1,100 @@
+/*
+ * Copyright (C) 2020-21 Application Library Engineering Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rilixtech.materialfancybuttons.slice;
 
-import com.rilixtech.fontawesome_typeface.FontAwesome;
-import com.rilixtech.materialfancybutton.MaterialFancyButton;
-import com.rilixtech.materialfancybuttons.ResourceTable;
+import static ohos.agp.components.ComponentContainer.LayoutConfig.MATCH_CONTENT;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.agp.components.DirectionalLayout;
+import com.rilixtech.community_material_typeface.CommunityMaterial;
+import com.rilixtech.devicon_typeface.Devicon;
+import com.rilixtech.dripicons_typeface.Dripicons;
+import com.rilixtech.entypo_typeface.Entypo;
+import com.rilixtech.fontawesome_typeface.FontAwesome;
+import com.rilixtech.foundation_icons_typeface.FoundationIcons;
+import com.rilixtech.glyphicons_halflings_typeface.GlyphiconsHalflings;
+import com.rilixtech.google_material_typeface.GoogleMaterial;
+import com.rilixtech.ionicons_typeface.Ionicons;
+import com.rilixtech.material_design_iconic_typeface.MaterialDesignIconic;
+import com.rilixtech.materialfancybutton.MaterialFancyButton;
+import com.rilixtech.materialfancybutton.typeface.IIcon;
+import com.rilixtech.materialfancybuttons.ResourceTable;
+import com.rilixtech.meteocons_typeface.Meteocons;
+import com.rilixtech.mfglabs_iconset_typeface.MFGLabsIconset;
+import com.rilixtech.mobirise_icons_typeface.MobiriseIcons;
+import com.rilixtech.octicons_typeface.Octicons;
+import com.rilixtech.open_iconic_typeface.OpenIconic;
+import com.rilixtech.picol_typeface.Picol;
+import com.rilixtech.pixeden_7_stroke_typeface.Pixeden7Stroke;
+import com.rilixtech.themify_icons_typeface.ThemifyIcons;
+import com.rilixtech.typicons_typeface.Typicons;
+import com.rilixtech.vaadin_icons_typeface.VaadinIcons;
+import com.rilixtech.weather_icons_typeface.WeatherIcons;
 
-import static ohos.agp.components.ComponentContainer.LayoutConfig.MATCH_CONTENT;
-
+/**
+ * This AbilitySlice displays a set of {@link MaterialFancyButton} Components instantiated using Java code
+ * on a blank layout.
+ */
 public class ProgramButtonsSlice extends AbilitySlice {
+
+    private static final String BUTTON_TEXT_FORMAT = "%s Button";
 
     @Override
     protected void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_program_buttons);
 
-        MaterialFancyButton fontAwesomeBtn = new MaterialFancyButton(this);
-        fontAwesomeBtn.setIcon(FontAwesome.Icon.FAWI_ADDRESS_BOOK);
-        fontAwesomeBtn.setRadius(30);
+        addAllModuleButtons();
+    }
 
+    private void addAllModuleButtons() {
+        addButtonToLayout(CommunityMaterial.Icon.CMDI_LIGHTBULB);
+        addButtonToLayout(Devicon.Icon.DEVI_UBUNTU_PLAIN);
+        addButtonToLayout(Dripicons.Icon.DRPI_ARCHIVE);
+        addButtonToLayout(Entypo.Icon.ENTI_MESSAGE);
+        addButtonToLayout(FontAwesome.Icon.FAWI_ADDRESS_BOOK);
+        addButtonToLayout(FoundationIcons.Icon.FOUI_ALERT);
+        addButtonToLayout(GlyphiconsHalflings.Icon.GLYI_CAMERA);
+        addButtonToLayout(GoogleMaterial.Icon.GMDI_LOCATION_ON);
+        addButtonToLayout(Ionicons.Icon.IONI_IOS_INFINITE);
+        addButtonToLayout(MaterialDesignIconic.Icon.MDII_AIRPLAY);
+        addButtonToLayout(Meteocons.Icon.METI_SNOW);
+        addButtonToLayout(MFGLabsIconset.Icon.MFGI_ICON_GLOBE);
+        addButtonToLayout(MobiriseIcons.Icon.MBRI_DESKTOP);
+        addButtonToLayout(Octicons.Icon.OCTI_LINK);
+        addButtonToLayout(OpenIconic.Icon.OPIC_VIDEO);
+        addButtonToLayout(Picol.Icon.PICI_INTERNET);
+        addButtonToLayout(Pixeden7Stroke.Icon.PE7I_7S_FLAG);
+        addButtonToLayout(ThemifyIcons.Icon.THEI_PAINT_BUCKET);
+        addButtonToLayout(Typicons.Icon.TYPI_EJECT);
+        addButtonToLayout(VaadinIcons.Icon.VAAI_FOLDER);
+        addButtonToLayout(WeatherIcons.Icon.WICI_EARTHQUAKE);
+    }
 
-        DirectionalLayout.LayoutConfig fabLayoutConfig =
-                new DirectionalLayout.LayoutConfig(MATCH_CONTENT,
-                        MATCH_CONTENT);
+    private void addButtonToLayout(IIcon icon) {
+        String buttonText = String.format(BUTTON_TEXT_FORMAT, icon.getClass().getSimpleName());
+        MaterialFancyButton materialFancyButton = new MaterialFancyButton(this);
+        materialFancyButton.setIcon(icon);
+        materialFancyButton.setText(buttonText);
+        materialFancyButton.setRadius(30);
+        DirectionalLayout.LayoutConfig layoutConfig =
+                new DirectionalLayout.LayoutConfig(MATCH_CONTENT, MATCH_CONTENT);
         DirectionalLayout container = (DirectionalLayout) findComponentById(ResourceTable.Id_programbuttons_container);
-        container.addComponent(fontAwesomeBtn, fabLayoutConfig);
+        container.addComponent(materialFancyButton, layoutConfig);
     }
 
     @Override
